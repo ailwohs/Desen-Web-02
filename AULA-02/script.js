9d30b5ac34987b9f0f5b850645d62addcc41aa4c
ailwohs <juliaavancisoares@gmail.com>
2025-04-22 19:42:04 -0300

    adicionando atividades

---
+++ AULA-02/script.js
@@ -0,0 +1,19 @@
+
+//ajuestes//
+
+//let nome = "Julia";
+//console.log(`Meu nome é ${nome}`);
+
+
+//laranjas = laranjas + 1//
+//console.log(laranjas)
+//let quadrado = 4, triangulo = 3, retangulo= 4
+//console.log(quadrado*(triangulo*retangulo))
+
+
+var nome = 'Pedro'
+console.log(nome)
+const sobrenome = "Soares"
+console.log(sobrenome)
+var nome = "Miranda"
+const ip = "127.0.0.0.1"
